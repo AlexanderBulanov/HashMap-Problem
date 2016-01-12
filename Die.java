9a0460e17a2d96ed5336fda1f9815d0a6151aa9f
AlexanderBulanov <alexanderbulanovcodes@gmail.com>
2016-01-12 09:23:27 -0800

    Added a bunch of code
    May need more testing

--- Die.java
+++ Die.java
@@ -1,1 +1,28 @@
+//Adam Barszczak, Alex Bulanov
+
+import java.math.*;
+
+public class Die {
+	int sidenum; //is this the appropriate field to store sidenum?
+	int rollresult = 0;
+	
+	public Die(int sidenum) {
+		this.sidenum = sidenum;
+	}
+
+	public void roll() {
+		rollresult = (int) (Math.random() * (sidenum)) + 1;
+	}
+	
+	public int getValue() {
+		return rollresult;
+	}
+	
+	public static void main(String[] args) {
+		Die d = new Die(6);
+		d.roll();
+		System.out.println(d.getValue());
+	}
+	
+}
 
