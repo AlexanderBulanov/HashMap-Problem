a1e25f26b0eff383c34347ba693ed50c1902cc7c
AlexanderBulanov <alexanderbulanovcodes@gmail.com>
2016-01-12 09:46:01 -0800

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,1 +1,14 @@
+import javax.swing.JFrame;
 
+public class Display extends JFrame {
+	int[] histostorage;
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void histogram() {
+		int[] rollresults = new int [1000];
+	}
+
+}
